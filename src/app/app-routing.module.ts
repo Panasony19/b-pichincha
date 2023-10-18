e9b4958ee2d252d489c868566b00bf3d11bcc80d
aalvarado2 <aalvarado@ole.agency>
2023-10-17 20:57:48 -0400

    Updated version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent,
+  },
+  { 
+    path: 'create-product', 
+    component: CreateProductComponent,
+  },
+  { 
+    path: 'update-product', 
+    component: CreateProductComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
